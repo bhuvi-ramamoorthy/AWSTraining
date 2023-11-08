539be4e776abd6fc0671d79dc034341d4880e9f5
bhuvi-ramamoorthy <bhuvana.garimalla@gmail.com>
2023-11-08 10:31:55 -0800

    comment changes

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ provider "aws" {
 resource "aws_instance" "web_app" {
     instance_type ="t2.micro"
     ami = "ami-010f8b02680f80998"
+    #name = "can i choose a name for ec2 instance"
     subnet_id = "subnet-0221e76c43c58744a"
 }
 #making changes to configure access token
