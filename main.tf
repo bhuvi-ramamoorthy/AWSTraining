f076bfe434ea510c609a326383426eae974b84b0
bhuvi-ramamoorthy <bhuvana.garimalla@gmail.com>
2023-11-08 10:29:07 -0800

    comment changes

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 terraform {
     required_providers {
         aws = {
@@ -9,5 +10,14 @@ terraform {
 }
 
 provider "aws" {
-    region= "us-west-2"
+    access_key = var.AWS_ACCESS_KEY
+    secret_key = var.AWS_SECRET_KEY
+    region= "us-west-1"
 }
+
+resource "aws_instance" "web_app" {
+    instance_type ="t2.micro"
+    ami = "ami-010f8b02680f80998"
+    subnet_id = "subnet-0221e76c43c58744a"
+}
+#making changes to configure access token
